e6a4b654ab70a70e20f6793cad5d7186b7905cd5
MoritzRS <moritz.r.schulz@gmail.com>
2023-04-03 18:18:37 +0200

    added simple arch install script

---
+++ install.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+######### Prepare Setup ##########
+loadkeys de-latin1;
+timedatectl set-ntp true;
+reflector \
+    --latest 5 \
+    --sort rate \
+    --country Austria,France,Germany,Switzerland \
+    --save /etc/pacman.d/mirrorlist;
+pacman -Syy --needed --noconfirm git wget unzip;
+
+########## Disk Setup ##########
+parted /dev/sda -- mklabel gpt;
+parted /dev/sda -- mkpart ESP fat32 1MB 512MB;
+parted /dev/sda -- mkpart primary 512MB -20GB;
+parted /dev/sda -- mkpart primary linux-swap -20GB 100%;
+parted /dev/sda -- set 1 esp on;
+
+mkfs.fat -F 32 -n boot /dev/sda1;
+mkfs.ext4 -L nixos /dev/sda1;
+mkswap -L swap /dev/sda3;
+
+mount /dev/disk/by-label/nixos /mnt;
+mkdir -p /mnt/boot;
+mount /dev/disk/by-label/boot /mnt/boot;
+swapon /dev/disk/by-label/swap;
+
+
+########## Base Install #########
+pacstrap /mnt base base-devel linux linux-firmware;
+genfstab -U /mnt >> /mnt/etc/fstab;
+
+
+########## Time Setup ##########
+arch-chroot /mnt ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime;
+arch-chroot /mnt hwclock --systohc;
+
+
+######### Locale Setup #########
+echo "LANG=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_ADDRESS=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_IDENTIFICATION=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_MEASUREMENT=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_MONETARY=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_NAME=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_NUMERIC=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_PAPER=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_TELEPHONE=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+echo "LC_TIME=de_DE.UTF-8" >> /mnt/etc/locale.conf;
+
+echo "KEYMAP=de" >> /mnt/etc/vconsole.conf;
+echo "FONT=" >> /mnt/etc/vconsole.conf;
+echo "FONT_MAP=" >> /mnt/etc/vconsole.conf;
+
+echo "# Autoinstaller" >> /mnt/etc/locale.gen;
+echo "de_DE.UTF-8 UTF-8" >> /mnt/etc/locale.gen;
+echo "en_US.UTF-8 UTF-8" >> /mnt/etc/locale.gen;
+arch-chroot /mnt locale-gen;
+
+
+########## Host Setup #########
+echo "archlinux" > /mnt/etc/hostname;
+echo "127.0.0.1    localhost" >> /mnt/etc/hosts;
+echo "::1          localhost" >> /mnt/etc/hosts;
+echo "127.0.1.1    archlinux.localdomain    archlinux" >> /mnt/etc/hosts;
+
+
+########## Bootloader Setup ##########
+arch-chroot /mnt pacman -S grub-efi-x86_64 efibootmgr dosfstools os-prober mtools --needed --noconfirm;
+arch-chroot /mnt grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck;
+arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg;
+
+
+########## Services ##########
+arch-chroot /mnt pacman -S --needed --noconfirm networkmanager dhcpcd acpid gnome-keyring;
+arch-chroot /mnt systemctl enable NetworkManager;
+arch-chroot /mnt systemctl enable dhcpcd;
+arch-chroot /mnt systemctl enable acpid;
+arch-chroot /mnt systemctl enable gnome-keyring;
+
+
+########## Sound ##########
+arch-chroot /mnt pacmans -S --needed --noconfirm pipewire
+
+
+########## Desktop ##########
+arch-chroot /mnt pacman -S --needed --noconfirm xorg-drivers gdm gnome-shell gnome-terminal gnome-control-center gnome-tweak-tool gnome-software gnome-calendar xdg-user-dirs nautilus;
+arch-chroot /mnt systemctl enable gdm;
+
+
+########## Fonts ###########
+arch-chroot /mnt pacman -S --needed --noconfirm ttf-hack-nerd ttf-sourcecodepro-nerd ttf-terminus-nerd;
+
+
+########## Shell ##########
+arch-chroot /mnt pacman -S --needed --noconfirm zsh starship;
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions /mnt/usr/local/zsh-plugins/zsh-autosuggestions;
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git /mnt/usr/local/zsh-plugins/zsh-syntax-highlighting;
+
+mkdir -p /mnt/etc/skel/.config;
+arch-chroot /mnt starship preset tokyo-night > /etc/skel/.config/starship.toml;
+
+mkdir -p /mnt/etc/skel;
+echo "HISTFILE=~/.zhistory" >> /mnt/etc/skel/.zshrc;
+echo "HISTSIZE=1000" >> /mnt/etc/skel/.zshrc;
+echo "SAVEHIST=1000" >> /mnt/etc/skel/.zshrc;
+echo "export NVM_DIR=\"/usr/local/nvm\"" >> /mnt/etc/skel/.zshrc;
+echo "[ -s \"$NVM_DIR/nvm.sh\" ] && \. \"$NVM_DIR/nvm.sh\"" >> /mnt/etc/skel/.zshrc;
+echo "[ -s \"$NVM_DIR/bash_completion\" ] && \. \"$NVM_DIR/bash_completion\"" >> /mnt/etc/skel/.zshrc;
+echo "alias ls=\"ls --color=auto\"" >> /mnt/etc/skel/.zshrc;
+echo "source /usr/local/zsh-plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> /mnt/etc/skel/.zshrc;
+echo "source /usr/local/zsh-plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> /mnt/etc/skel/.zshrc;
+echo "eval \"$(starship init zsh)\"" >> /mnt/etc/skel/.zshrc;
+
+########## Tools ##########
+arch-chroot /mnt pacman -S --needed --noconfirm php php-sqlite git wget neovim docker flatpak xdg-desktop-portal-gnome;
+
+# PHP
+sed -i s/\;extension=pdo_sqlite/extension=pdo_sqlite/ /mnt/etc/php/php.ini
+sed -i s/\;extension=sqlite3/extension=sqlite3/ /mnt/etc/php/php.ini
+
+# NVM
+local NVM_DIR="/mnt/usr/local/nvm";
+git clone https://github.com/nvm-sh/nvm.git ${NVM_DIR};
+cd ${NVM_DIR};
+git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`;
+\. ${NVM_DIR}/nvm.sh;
+chmod 777 ${NVM_DIR};
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+nvm install 18;
+
+
+########## User ##########
+mkdir -p /mnt/etc/skel/{Bilder,Dev,Dokumente,Downloads,Musik,Videos}
+arch-chroot /mnt pacman -S --needed --noconfirm sudo;
+arch-chroot /mnt useradd -m -g users -G wheel,storage,disk,power,audio,video,docker -s /usr/bin/zsh mrs;
+arch-chroot /mnt echo -e "1234\n1234" | passwd;
+arch-chroot /mnt echo -e "1234\n1234" | passwd mrs;
+echo "%wheel ALL=(ALL) ALL" > /mnt/etc/sudoers.d/10-installer;
+
+########## Finish ##########
+shutdown now;
--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,10 @@
 sudo pacman -Syyu
-sudo pacman -S neovim wget git php sqlitebrowser firefox godot blender
+sudo pacman -S sqlitebrowser firefox epiphany godot blender
 
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y flathub com.google.Chrome
-flatpak install -y flathub org.gnome.gitlab.somas.Apostrophe
-flatpak install -y flathub md.obsidian.Obsidian
-flatpak install -y flathub rest.insomnia.Insomnia
-flatpak install -y flathub com.visualstudio.code-oss
-flatpak install -y flathub org.remmina.Remmina
+flatpak install -y --user flathub com.google.Chrome
+flatpak install -y --user flathub org.gnome.gitlab.somas.Apostrophe
+flatpak install -y --user flathub md.obsidian.Obsidian
+flatpak install -y --user flathub rest.insomnia.Insomnia
+flatpak install -y --user flathub com.visualstudio.code-oss
+flatpak install -y --user flathub org.remmina.Remmina
