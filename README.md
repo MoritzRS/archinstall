090221c38d912c2ae17712983264f44a4e2ef2f0
MoritzRS <moritz.r.schulz@gmail.com>
2023-04-03 18:21:03 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# How To Install
+
+- On Live ISO `bash <(curl -s https://raw.githubusercontent.com/MoritzRS/archinstall/master/install.sh)`
+- On installed System `bash <(curl -s https://raw.githubusercontent.com/MoritzRS/archinstall/master/postinstall.sh)`
--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,10 @@
-sudo pacman -Syyu
-sudo pacman -S sqlitebrowser firefox epiphany godot blender
+sudo pacman -Syyu;
+sudo pacman -S --needed --noconfirm sqlitebrowser firefox epiphany godot blender;
 
-flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install -y --user flathub com.google.Chrome
-flatpak install -y --user flathub org.gnome.gitlab.somas.Apostrophe
-flatpak install -y --user flathub md.obsidian.Obsidian
-flatpak install -y --user flathub rest.insomnia.Insomnia
-flatpak install -y --user flathub com.visualstudio.code-oss
-flatpak install -y --user flathub org.remmina.Remmina
+flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo;
+flatpak install -y --user flathub com.google.Chrome;
+flatpak install -y --user flathub org.gnome.gitlab.somas.Apostrophe;
+flatpak install -y --user flathub md.obsidian.Obsidian;
+flatpak install -y --user flathub rest.insomnia.Insomnia;
+flatpak install -y --user flathub com.visualstudio.code-oss;
+flatpak install -y --user flathub org.remmina.Remmina;
